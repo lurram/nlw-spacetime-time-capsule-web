dd85cb22f4055817f0e76022cdfb012f8c5f1e98
lurram <lurramsantos.00.ls@gmail.com>
2023-05-24 23:03:42 -0300

    NLW Spacetime web

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '_IP_PC_', 'localhost'],
+  },
+}
+
+module.exports = nextConfig
